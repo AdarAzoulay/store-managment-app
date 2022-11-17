ca8567648e128cca22690e31bfce1b5b1ab55824
Adar Azoulay <12azoulay12@gmail.com>
2022-11-17 10:55:05 +0200

    Creating the angular application

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'managment';
+  users : any;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() : void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      res=>{ this.users =res}, err=> {console.log(err)}, ()=>{console.log("comleted")}
+    )
+  }
+}
